096c052fca8243994e5ea02a181576f24b4f8411
Tagussan <jap.yoshi@gmail.com>
2018-05-13 12:59:33 +0900

    fixed path problem

--- speedLearning.py
+++ speedLearning.py
@@ -6,22 +6,24 @@ import tempfile
 fpath = os.path.abspath(sys.argv[1])
 fbasename = os.path.basename(fpath)
 fname, fext = os.path.splitext(fbasename)
-fdir = os.path.dirname(fpath)
-
-fpath = "\"%s\"" % fpath
+outpath = os.path.abspath(sys.argv[2])
 
 with tempfile.TemporaryDirectory() as tmpdir:
-    tmpwav = "\"%s/%s.wav\"" % (tmpdir, fname)
-    subprocess.run("sox %s %s" % (fpath, tmpwav), shell=True)
+    tmpwav = os.path.join(tmpdir, "%s.wav" % fname)
+    args = ["sox", fpath, tmpwav]
+    subprocess.run(args)
 
-    tmpwav_fast = "\"%s/%s_fast.wav\"" % (tmpdir, fname)
-    subprocess.run("soundstretch %s %s -tempo=+90" % (tmpwav, tmpwav_fast), shell=True)
+    tmpwav_fast = os.path.join(tmpdir, "%s_fast.wav" % fname)
+    args = ["sonic", "-s", "1.1", tmpwav, tmpwav_fast]
+    subprocess.run(args)
 
-    tmpwav_superfast = "\"%s/%s_superfast.wav\"" % (tmpdir, fname)
-    subprocess.run("soundstretch %s %s -tempo=+200" % (tmpwav, tmpwav_superfast), shell=True)
+    tmpwav_superfast = os.path.join(tmpdir, "%s_superfast.wav" % fname)
+    args = ["sonic", "-s", "1.8", tmpwav, tmpwav_superfast]
+    subprocess.run(args)
 
-    tmpwav_joined = "\"%s/%s_concat.wav\"" % (tmpdir, fname)
-    subprocess.run("sox %s %s %s %s" % (tmpwav_fast, tmpwav_superfast, tmpwav_fast, tmpwav_joined), shell=True)
+    tmpwav_joined = os.path.join(tmpdir, "%s_joined.wav" % fname)
+    args = ["sox", tmpwav_fast, tmpwav_superfast, tmpwav_fast, tmpwav_joined]
+    subprocess.run(args)
 
-    newmp3 = "\"%s/%s_fast.mp3\"" % (fdir, fname)
-    subprocess.run("lame -V1 %s %s" % (tmpwav_joined, newmp3), shell=True)
+    args = ["lame", "-V1", tmpwav_joined, outpath]
+    subprocess.run(args)
